9ec66a262feb7b15325faada6700c74ec8cd3a94
Ivanlyin1917 <52660349+IvankoIlyin@users.noreply.github.com>
2024-04-30 15:24:16 +0300

    .

---
+++ mic/lab6/main.py
@@ -0,0 +1,14 @@
+import mnist as mnist
+import numpy as np
+import pandas as pd
+import sklearn
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import struct
+from array import array
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_openml
+
+
+
---
+++ mic/lab8/main.py
@@ -0,0 +1,14 @@
+import os
+import sklearn
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from graphviz import Source
+from sklearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeClassifier, export_graphviz, plot_tree
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.datasets import make_moons
+
+
+
